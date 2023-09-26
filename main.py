20aef6c42b30f328aaaefe62ba99601b581b7c90
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2023-09-26 13:01:39 +0530

    Completed a working todo app  added data base, add, update, delete, form actions, jinja templating  + Dynamic rendering

--- main.py
+++ main.py
@@ -5,9 +5,38 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MADIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+class TODO(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    todoList = TODO.query.all()
+    return render_template("index.html", todoList=todoList)
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    newTodo = TODO(title=title, complete=False)
+    db.session.add(newTodo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/update/<int:todoId>")
+def update(todoId):
+    item = TODO.query.filter_by(id=todoId).first()
+    item.complete = not item.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todoId>")
+def delete(todoId):
+    item = TODO.query.filter_by(id=todoId).first()
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     with app.app_context():
