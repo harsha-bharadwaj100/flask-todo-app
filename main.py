394bc62880354d606ee7bbbd4765765537fecc3d
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2023-09-26 11:24:29 +0530

    Set database + Form

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello, world!")
+from flask import Flask, redirect, request, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MADIFICATIONS'] = False
+db = SQLAlchemy(app)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
